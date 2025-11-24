204c75ef3f98891b484eeefe0e6751bfe799d249
JoaquinPar <jparedes.peake@gmail.com>
2025-11-23 19:07:08 -0800

    Main function is now in a while loop.

--- src/code/Main.java
+++ src/code/Main.java
@@ -1,28 +1,76 @@
+import java.util.Scanner;
+
 public class Main
 {
     public static void main(final String[] args)
     {
+        final Scanner scanner;
         final Calculator calc;
         final CommandStack stack;
+        final int initialNum;
 
-        calc = new Calculator(0);
+        scanner = new Scanner(System.in);
         stack = new CommandStack();
 
-        stack.execute(new AddCommand(calc, 5));
-        System.out.println(calc.getValue());
+        System.out.print("Enter an initial number for the calculator: ");
+        initialNum = scanner.nextInt();
 
-        stack.undo();
-        System.out.println(calc.getValue());
+        calc = new Calculator(initialNum);
 
-        stack.execute(new AddCommand(calc, 30));
-        stack.execute(new MultiplyCommand(calc, 3));
-        System.out.println(calc.getValue());
+        System.out.println("Type commands like: 5 + | 10 * | 3 / | undo | quit");
 
-        stack.execute(new DivideCommand(calc, 2));
-        stack.execute(new SubtractCommand(calc, 5));
-        System.out.println(calc.getValue());
+        while (true)
+        {
+            final double num;
+            final String op;
+            final String operation;
+            final Command cmd;
 
-        stack.undo();
-        System.out.println(calc.getValue());
+            System.out.print("> ");
+
+            if (!scanner.hasNextDouble())
+            {
+                op = scanner.next();
+
+                if (op.equalsIgnoreCase("undo"))
+                {
+                    stack.undo();
+                    System.out.println("= " + calc.getValue());
+                    continue;
+                }
+
+                if (op.equalsIgnoreCase("quit"))
+                {
+                    System.out.println("Goodbye!");
+                    break;
+                }
+
+                System.out.println("Invalid input.");
+                continue;
+            }
+
+            num = scanner.nextDouble();
+            operation = scanner.next();
+
+            cmd = switch (operation)
+            {
+                case "+" -> new AddCommand(calc, num);
+                case "-" -> new SubtractCommand(calc, num);
+                case "*" -> new MultiplyCommand(calc, num);
+                case "/" -> new DivideCommand(calc, num);
+                default -> null;
+            };
+
+            if (cmd == null)
+            {
+                System.out.println("Unknown operation.");
+                continue;
+            }
+
+            stack.execute(cmd);
+            System.out.println("= " + calc.getValue());
+        }
+
+        scanner.close();
     }
 }
